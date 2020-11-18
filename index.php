73c054953b2fe29b0ad62d1a3c59bbc9310291d3
charoenlap <charoenlap88@gmail.com>
2020-11-18 13:55:53 +0700

    update

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
 	require_once('required/main_function.php');
 	require_once('catalog/setup.php'); 
 	require_once('system/loader/autoload.php'); 
-	echo "s";exit();
 	// $route = array(
 	// 	'login'
 	// );
